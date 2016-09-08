c1caaa48fe07cb683029ed5ade1d871d06d7db27
CastellaFactory <castellafactory@gmail.com>
2016-09-08 12:07:12 +0900

    use ghq list output

--- src/ghqup.rs
+++ src/ghqup.rs
@@ -1,24 +1,18 @@
-extern crate wait_group;
-use self::wait_group::WaitGroup;
+extern crate num_cpus;
+
+use Args;
+
+use scoped_pool::Pool;
 
 use std::ffi::OsStr;
-use std::fs;
-use std::io::StdoutLock;
-use std::io::Write;
-use std::io;
-use std::path::Path;
-use std::path::PathBuf;
+use std::io::{self, BufReader, BufRead, Write, StdoutLock};
+use std::path::{Path, PathBuf};
 use std::process;
-use std::str;
-use std::thread;
-use std::vec::Vec;
-use super::Args;
 
 #[derive(Clone)]
 pub struct Ghqup {
     root: PathBuf,
     args: Args,
-    wg: WaitGroup,
 }
 
 impl Ghqup {
@@ -26,134 +20,63 @@ impl Ghqup {
         Ghqup {
             root: PathBuf::from(path),
             args: args,
-            wg: WaitGroup::new(),
         }
     }
 
     pub fn run(&self) {
-        if self.args.flag_github || self.args.flag_all {
-            self.exec_user_name_loop("github.com");
-        }
-        if self.args.flag_bitbucket || self.args.flag_all {
-            self.exec_user_name_loop("bitbucket.org");
-        }
-    }
+        let res = process::Command::new("ghq")
+            .arg("list")
+            .output()
+            .expect("failed to execute process 'ghq list'");
 
-    fn exec_user_name_loop(&self, repo_type: &str) {
-        match fs::read_dir(self.root.join(repo_type)) {
-            Ok(files) => {
-                for file in files.into_iter().filter_map(|e| e.ok()) {
-                    match file.metadata() {
-                        Ok(metadata) => {
-                            if metadata.is_dir() {
-                                match file.path()
-                                    .file_name()
-                                    .and_then(|name| name.to_str()) {
-                                    Some(username) => {
-                                        let n_self = self.clone();
-                                        let repo_type = repo_type.to_owned();
-                                        let username = username.to_owned();
-                                        self.wg.add(1);
-                                        thread::spawn(move || {
-                                            n_self.exec_repo_loop(repo_type, username);
-                                        });
-                                    }
-                                    None => return,
-                                };
-                            }
-                        }
-                        Err(err) => {
-                            println!("{}", err.to_string());
-                            return;
-                        }
+        let reader = BufReader::new(&res.stdout[..]);
+
+        let pool = Pool::new(num_cpus::get());
+
+        pool.scoped(move |scoped| {
+            for line in reader.lines() {
+                match line {
+                    Ok(line) => {
+                        let g = self.clone();
+                        scoped.execute(move || {
+                            g.exec_repo_loop(line);
+                        });
+
                     }
+                    Err(_) => continue,
                 }
             }
-            Err(err) => {
-                println!("{}: {}",
-                         self.root.join(repo_type).to_str().unwrap_or(""),
-                         err.to_string());
-            }
-        }
-        self.wg.wait();
+        });
     }
 
-    fn exec_repo_loop(&self, repo_type: String, username: String) {
-        defer!(self.wg.done());
-        match fs::read_dir(self.root.join(&repo_type).join(&username)) {
-            Ok(files) => {
-                for file in files.into_iter().filter_map(|e| e.ok()) {
-                    match file.metadata() {
-                        Ok(metadata) => {
-                            if metadata.is_dir() {
-                                match file.path()
-                                    .file_name()
-                                    .and_then(|name| name.to_str()) {
-                                    Some(repo) => {
-                                        let n_self = self.clone();
-                                        let repo_type = repo_type.clone();
-                                        let username = username.clone();
-                                        let repo = repo.to_owned();
-                                        self.wg.add(1);
-                                        thread::spawn(move || {
-                                            n_self.update(repo_type, username, repo, 0);
-                                        });
-                                    }
-                                    None => {
-                                        return;
-                                    }
-                                };
-                            }
-                        }
-                        Err(err) => {
-                            println!("{}", err.to_string());
-                            return;
-                        }
-                    };
-                }
-            }
-            Err(err) => {
-                println!("{}: {}",
-                         self.root.join(repo_type).join(username).to_str().unwrap_or(""),
-                         err.to_string());
-            }
-        }
+    fn exec_repo_loop(&self, repo: String) {
+        self.update(repo, 0);
     }
 
-
-    fn update(&self, repo_type: String, username: String, repo: String, count: u32) {
-        defer!(self.wg.done());
+    fn update(&self, repo: String, count: u32) {
         let res = process::Command::new("git")
             .arg("pull")
-            .current_dir(self.root.join(&repo_type).join(&username).join(&repo))
+            .current_dir(self.root.join(&repo))
             .output()
             .expect("failed to execute process 'git pull'");
 
         if res.status.success() {
             let stdout = io::stdout();
             let mut lock = stdout.lock();
-            writeln!(lock,
-                     "{}: \x1b[33mDone\x1b[0m",
-                     Path::new(&repo_type).join(&username).join(&repo).display())
-                .unwrap();
+            writeln!(lock, "{}: \x1b[33mDone\x1b[0m", Path::new(&repo).display()).unwrap();
             self.print_output(&mut lock, res.stdout);
-        } else if count < self.args.flag_retry {
-            self.wg.add(1);
-            self.update(repo_type, username, repo, count + 1);
-            return;
+        } else if count < self.args.retry {
+            self.update(repo, count + 1);
         } else {
             let stdout = io::stdout();
             let mut lock = stdout.lock();
-            writeln!(lock,
-                     "{}: \x1b[31mError\x1b[0m",
-                     Path::new(&repo_type).join(&username).join(&repo).display())
-                .unwrap();
+            writeln!(lock, "{}: \x1b[31mError\x1b[0m", Path::new(&repo).display()).unwrap();
             self.print_output(&mut lock, res.stderr);
         }
     }
 
     fn print_output(&self, lock: &mut StdoutLock, v: Vec<u8>) {
-        if !self.args.flag_quiet {
+        if !self.args.quiet {
             match String::from_utf8(v) {
                 Ok(s) => writeln!(lock, "{}", s).unwrap(),
                 _ => {}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,51 @@
-#[macro_use(defer)]
-extern crate scopeguard;
-extern crate docopt;
-extern crate rustc_serialize;
+#[macro_use]
+extern crate clap;
+extern crate scoped_pool;
 
-use docopt::Docopt;
+use clap::{Arg, App};
+
 use std::error::Error;
 use std::process;
 
 mod ghqup;
 
-const USAGE: &'static str = "
-Usage:
-  ghqup [--github] [--bitbucket] [--all] [--quiet] [--retry <count>]
-  ghqup [--help]
-  
-Options:
-  -h, --help                    Show this screen.
-  -g, --github                  Update GitHub repos.
-  -b, --bitbucket               Update Bitbucket repos.
-  -a, --all                     Update All repos.
-  -q, --quiet                   Quiet mode.
-  -r <count>, --retry <count>   Retry count [default: 5].
-";
-
-#[derive(Debug, RustcDecodable, Clone)]
+#[derive(Debug, Clone)]
 pub struct Args {
-    flag_github: bool,
-    flag_bitbucket: bool,
-    flag_all: bool,
-    flag_quiet: bool,
-    flag_retry: u32,
+    quiet: bool,
+    retry: u32,
 }
-
 
 fn main() {
     let res = process::Command::new("ghq")
         .arg("root")
-        .output();
+        .output()
+        .expect("failed to execute ghq root");
 
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+    let m = App::new("ghqup")
+        .version(crate_version!())
+        .version_short("v")
+        .arg(Arg::with_name("QUIUT")
+            .long("quiet")
+            .short("q")
+            .help("Quiet mode"))
+        .arg(Arg::with_name("RETRY")
+            .long("retry")
+            .short("r")
+            .takes_value(true)
+            .empty_values(false)
+            .value_name("RETRY")
+            .help("Retry count [default: 3]"))
+        .get_matches();
 
-    match res {
-        Ok(output) => {
-            match String::from_utf8(output.stdout) {
-                Ok(root) => {
-                    let ghqup = ghqup::Ghqup::new(root.trim_right(), args);
-                    ghqup.run();
-                }
-                Err(err) => panic!("{}", err.description()),
-            };
-
-        }
-        Err(_) => {
-            println!("failed to run ghq.");
-            std::process::exit(1);
-        }
+    let args = Args {
+        quiet: m.is_present("QUIET"),
+        retry: value_t!(m, "RETRY", u32).unwrap_or(3),
     };
+
+    match String::from_utf8(res.stdout) {
+        Ok(root) => {
+            ghqup::Ghqup::new(root.trim_right(), args).run();
+        }
+        Err(err) => panic!("{}", err.description()),
+    }
 }
